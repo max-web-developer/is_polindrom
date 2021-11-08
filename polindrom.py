dc8dbe5d2eae6e680d354265e979983a37335001
Max_Kavlanov <maxkavlanov110@gmail.com>
2021-11-08 17:03:50 +0500

    Polindrom

---
+++ polindrom.py
@@ -0,0 +1,23 @@
+# word= str(input('enter word:'))
+# w = len(word)
+# i = 0
+# w = w - 1
+# k = 0
+# while w - i >= i:
+#     if word[w - i] == word[i]: # Проверяем слово на полиндром
+#         i += 1 
+#     else:
+#         k = 1
+#         break
+# if k == 1:
+#   print("Not Polindrom")
+# else:
+#   print("Polindrom")
+
+word = str(input('Enter word: '))
+a = word[::-1] # Проверяем на полидром через последний индекс
+if word == a:
+  print("Polindrom")
+else:
+  print("Not Polindrom")
+  
